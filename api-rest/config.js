bf9bbe885dd9871d3f7e3dcfb7da36750b5cfccc
mazcui <mazcui@motortale.com>
2020-07-04 16:35:37 -0300

    changing dependences

--- api-rest/config.js
+++ api-rest/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/recipedb'
+    db: "mongodb://<dbuser>:<dbpassword>@ds053419.mlab.com:53419/heroku_shqschsp" || 'mongodb://localhost:27017/recipedb'
 }
